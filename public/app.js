e0de055611f2dd341f2b3e5a2e7e179ee1074fb2
Simon Last <stlast1@gmail.com>
2013-09-08 17:24:35 -0700

    cleanup folders.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,30 @@
+// Setup db interface
+(function(){
+  var db = {};
+  var socket = io.connect(window.location.href);
+
+  db.get = function(id, callback){
+    console.log("get: ", id);
+    socket.emit("get", {id: id}, function(value){
+      callback(value.value, value.err);
+    });
+  };
+
+  db.set = function(id, value){
+    console.log("set: ", id);
+    var data = {
+      id: id,
+      value: value
+    };
+    socket.emit("set", data);
+  };
+
+
+  window.db = db;
+})();
+
+
+// Setup require interface.
 (function(){
 
   var tryEval = function(str){
