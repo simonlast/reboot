3585e5d153eb6844f249bb55a3e68fc633833582
Simon Last <stlast1@gmail.com>
2014-02-01 13:21:07 -0800

    Cleanup.

--- server/server.js
+++ server/server.js
@@ -7,51 +7,50 @@ var http = require("http"),
   share = require("share");
 
 
-//Share.js
-
-var shareServer = connect();
-
-
-
-
-// Main server
-
 var app = express();
 
+
+// Setup ShareJS
 var options = {db:{type:"redis"}};
 share.server.attach(app, options);
 
+
+// Templates
 app.engine("html", consolidate.underscore);
 app.set("view engine", "html");
 app.set("views", path.join(__dirname, "templates"));
 
 
+// Static
 var oneDay = 86400000;
-console.log(path.join(__dirname + "../public"));
 app.use(
   connect.static(path.join(__dirname + "/../public"), { maxAge: oneDay })
 );
 
+
+// Editor
 app.get("/edit/:file", function(req, res){
-
   res.render("edit.html");
-
 });
 
+
+// Viewer
 app.get("/:file", function(req, res){
   var file = req.params.file;
 
   app.model.getSnapshot(file, function(err, data){
+
     if((typeof data !== "undefined") && (typeof data.snapshot !== "undefined")){
       res.setHeader("Content-Type", "text/html");
       res.send(data.snapshot);
     }else{
       res.send(404);
     }
+
   });
 
 });
 
-var server = http.createServer(app);
 
+var server = http.createServer(app);
 server.listen(process.argv[2] || 80);
